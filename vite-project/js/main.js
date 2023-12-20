03d2695c264af0ce6a86bc625df72f715e264685
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-12-20 09:23:49 -0500

    asd

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,11 +1,41 @@
 import '../css/style.css'
 
-const DomSelectors = [
-    document.getElementById("app"),
-]
+const DomSelectors = {
+    apps : document.getElementById("app"),
+    authInput: document.getElementById("AuthInput"),
+    submit: document.getElementById("submit"),
+    form1 : document.getElementById("form1"),
+};
 
-const authorname = 'dickens'
+const authorname = "clare"
 const URL = `https://openlibrary.org/search.json?author=${authorname}&sort=new`
+
+/* function callVals(){
+    const authornames = DomSelectors.authInput.value;
+
+    return{
+        authorname: authornames,
+    };
+};
+
+DomSelectors.form1.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(callVals)
+    console.log("duhrrr")
+
+});
+ */
+
+
+function BookPrint(book){
+    const BookCard = `
+    <div class="BookCards">
+        <p>Novel: ${book.title}</p>
+        <p>Author: ${book.author_name}</p>
+        <p>Publish Date: ${book.publish_date}</p>
+    </div>`;
+    document.getElementById("app").insertAdjacentHTML("beforeend", BookCard);
+};
 
 async function getData(URL){
     try {
@@ -23,19 +53,9 @@ async function getData(URL){
     } catch (error) {
         console.log("No work");
     }
-}
+};
+
 getData(URL);
 
-function BookPrint(book){
-    const BookCard = `
-    <div class="BookCards">
-        <p>Novel: ${book.title}</p>
-        <p>Author: ${book.author_name}</p>
-        <p>Publish Date: ${book.publish_date}</p>
-
-
-    </div>
-    `;
-    document.getElementById("app").insertAdjacentHTML("beforeend", BookCard);
-};
+ 
 
