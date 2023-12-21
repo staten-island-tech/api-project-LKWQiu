543e561841a1201a4b34b9de52084df27bbc8016
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-12-21 09:11:26 -0500

    yhhhhhhhhh

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -7,55 +7,43 @@ const DomSelectors = {
     form1 : document.getElementById("form1"),
 };
 
-const authorname = "clare"
-const URL = `https://openlibrary.org/search.json?author=${authorname}&sort=new`
-
-/* function callVals(){
-    const authornames = DomSelectors.authInput.value;
-
-    return{
-        authorname: authornames,
-    };
-};
 
 DomSelectors.form1.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log(callVals)
+
+    const authorname = DomSelectors.authInput.value;
+    const URL = `https://openlibrary.org/search.json?author=${authorname}&sort=new`
+    console.log(URL)
+
     console.log("duhrrr")
 
-});
- */
-
-
-function BookPrint(book){
-    const BookCard = `
-    <div class="BookCards">
-        <p>Novel: ${book.title}</p>
-        <p>Author: ${book.author_name}</p>
-        <p>Publish Date: ${book.publish_date}</p>
-    </div>`;
-    document.getElementById("app").insertAdjacentHTML("beforeend", BookCard);
-};
-
-async function getData(URL){
-    try {
-        const response = await fetch(URL);
-        if(response.status != 200) {
-            console.log("works")
+    function BookPrint(book){
+        const BookCard = `
+        <div class="BookCards">
+            <p>Novel: ${book.title}</p>
+            <p>Author: ${book.author_name}</p>
+            <p>Publish Date: ${book.publish_date}</p>
+        </div>`;
+        document.getElementById("app").insertAdjacentHTML("beforeend", BookCard);
+    };
+    
+    async function getData(URL){
+        try {
+            const response = await fetch(URL);
+            if(response.status != 200) {
+                console.log("works")
+            }
+            const data = await response.json();
+            console.log(data)
+    
+            data.docs.forEach(data => {
+            BookPrint(data)    
+            });
+    
+        } catch (error) {
+            console.log("No work");
         }
-        const data = await response.json();
-        console.log(data)
-
-        data.docs.forEach(data => {
-        BookPrint(data)    
-        });
-
-    } catch (error) {
-        console.log("No work");
-    }
-};
-
-getData(URL);
-
- 
-
+    };
+    
+    getData(URL);
+});
